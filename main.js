9adf0ba5d67d26b3dceb2c8d1d31faed2859c286
Henrique Stolle <henriquestolleh@gmail.com>
2024-04-15 19:59:39 -0300

    Commit 04: Parte de funcionalidade Feita. Projeto concluído.

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
 function fazerCalculo() {
+    var operacao = document.querySelector('select[name="viagem"]').value;
+    var numOne = parseFloat(document.getElementById('numOne').value);
+    var numTwo = parseFloat(document.getElementById('numTwo').value);
     
-};
+    var resultado;
+    switch(operacao) {
+        case 'soma':
+            resultado = numOne + numTwo;
+            break;
+        case 'subtracao':
+            resultado = numOne - numTwo;
+            break;
+        case 'divisao':
+            if (numTwo === 0) {
+                resultado = "Não é possível dividir um número por 0 (zero)";
+                break;
+            }
+            resultado = numOne / numTwo;
+            break;
+        case 'multiplicacao':
+            resultado = numOne * numTwo;
+            break;
+        default: 
+            resultado = "Coloque um Operador Válido.";
+            break;
+    }
+    console.log(operacao);
+    document.getElementById('span-resultado').textContent = resultado;
+}
