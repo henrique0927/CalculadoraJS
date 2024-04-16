b7acd2305be92ffe7139f5d40cad18028ab74c0e
HENRIQUE SILVA STOLLE <henrique_stolle@estudante.sesisenai.org.br>
2024-04-16 10:45:09 -0300

    Commit 05: adicionado comentários na lógica JS

--- main.js
+++ main.js
@@ -2,8 +2,13 @@ function fazerCalculo() {
     var operacao = document.querySelector('select[name="viagem"]').value;
     var numOne = parseFloat(document.getElementById('numOne').value);
     var numTwo = parseFloat(document.getElementById('numTwo').value);
-    
+    // Criação de variáveis para pegar cada elemento em que é colocado algum valor.
+
     var resultado;
+    // Variável para juntar o resultado entre os 1º e 2º números.
+
+    // Usando o swith para realizar uma ação de acordo com o valor obtido.
+    // na variável operação
     switch(operacao) {
         case 'soma':
             resultado = numOne + numTwo;
@@ -24,7 +29,12 @@ function fazerCalculo() {
         default: 
             resultado = "Coloque um Operador Válido.";
             break;
+            // Não executa o programa caso o usuário coloque uma opção inválida.
     }
     console.log(operacao);
+    // Mostrando o valor da operação no console.
+    // Para ter valor certeza de qual operação está sendo chamada. 
+
     document.getElementById('span-resultado').textContent = resultado;
+    // Selecionando o elemento <span> pelo ID e mostrando na tela o valor do resultado.
 }
